5fde2699e63f5b99a07b901643c9cb1074d74d34
SyedAliHamzaRizvi <alihamza168039@gmail.com>
2026-07-09 01:23:52 +0500

    changes

--- db.py
+++ db.py
@@ -8,6 +8,10 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL environment variable is missing")
+
+
 
 engine = create_engine(
     DATABASE_URL,
